69cc939822f80007263f8874d996055d5e1deb99
Michael Phan-Ba <michael@mikepb.com>
2015-06-07 10:41:18 -0700

    Added: Accept custom tags

--- test/sanitized_test.js
+++ test/sanitized_test.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const tag = 1;
+
 var assert = require("assert");
 var sanitize = require("gumbo-sanitize");
 var sanitized = require("../lib/sanitized");
@@ -8,6 +10,25 @@ describe("Default Sanitized HTML", function () {
   var dataType = sanitized();
   require("./sanitized")("#set", dataType);
   require("./common")(dataType);
+});
+
+describe("Default Sanitized HTML with custom tag", function () {
+  var customTag = Date.now();
+  var dataType = sanitized(null, customTag);
+  it("should sanitize mismatching tags on set", function () {
+    assert.deepEqual(dataType.set({
+      raw: "<script>Hello World!</script>",
+      html: "html",
+      tag: tag
+    }), {
+      type: "html",
+      val: {
+        raw: "<script>Hello World!</script>",
+        html: "Hello World!",
+        tag: customTag
+      }
+    });
+  });
 });
 
 describe("Relaxed Sanitized HTML", function () {
